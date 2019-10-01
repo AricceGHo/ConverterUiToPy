0bb44657a5a0216c1fe42d95dac1c04147c725a2
AricceGHo <44861119+AricceGHo@users.noreply.github.com>
2019-10-01 10:30:28 +0300

    Приведение кода в порядок

--- ConverterUbuntu.py
+++ ConverterUbuntu.py
@@ -10,6 +10,7 @@ from PyQt5.QtCore import Qt
 
 
 class ExampleApp(QtWidgets.QMainWindow, ConverterUbuntuUI.Ui_Form):
+
     def button_click_open(self):
         filters = "User Interface (*.ui)"
         f_dir = os.path.dirname(__file__)
@@ -18,19 +19,16 @@ class ExampleApp(QtWidgets.QMainWindow, ConverterUbuntuUI.Ui_Form):
         self.file_name_ui = file_name_ui[0]
 
     def new_convert(self):
+
         filters = "Py файлы (*.py)"
         if self.f_dir == "":
             self.f_dir = os.path.dirname(__file__)
-
         file_name_py = QtWidgets.QFileDialog.getSaveFileName(
             None, "Назовите файл", self.f_dir, filters)
         self.file_name_py = file_name_py[0]
         self.file_name_f, file_extention = os.path.splitext(self.file_name_py)
-
         if file_extention == '.py':
-
             self.file_name_f = self.file_name_py
-
         else:
             self.file_name_f = self.file_name_py+".py"
 
@@ -38,37 +36,34 @@ class ExampleApp(QtWidgets.QMainWindow, ConverterUbuntuUI.Ui_Form):
 
         if self.comb == "New":
             self.new_convert()
-
         bash_command = "pyuic5 %s -o %s" % (
             self.file_name_ui, self.file_name_py)
         os.system(bash_command)
         print("vse ok")
 
     def button_click_save(self):
-        try:
 
+        try:
             filters = "JSON файлы (*.json)"
             f_dir = os.path.dirname(__file__)
             file_name = QtWidgets.QFileDialog.getSaveFileName(
                 None, "Назовите файл настроек", f_dir, filters)
             file_name_f, file_extention = os.path.splitext(file_name[0])
-
             if file_extention == '.json':
                 file_name_f = file_name[0]
-
             else:
                 file_name_f = file_name[0]+".json"
-
             self.json_data_st = {"ui_data": self.json_data}
             with open(file_name_f, "w") as write_file:
-
                 json.dump(self.json_data_st, write_file)
 
         except Exception as e:
+
             print(e)
-            print('tuta')
+            print('tut')
 
     def button_click_load(self):
+
         try:
             self.comboBox.addItem("New")
             filters = "JSON файлы (*.json)"
@@ -77,20 +72,19 @@ class ExampleApp(QtWidgets.QMainWindow, ConverterUbuntuUI.Ui_Form):
                 None, "Выберете файл настроек", f_dir, filters)
             with open(file_name[0], 'r') as read_file:
                 self.settings = json.load(read_file)
-
             for i in range(len(self.settings["ui_data"])):
                 self.comboBox.addItem(self.settings["ui_data"][i]["name"])
-
             self.save_last_settings(file_name[0])
-
         except Exception as e:
             print(e)
 
     def same_folder_checkbox(self, state):
+
         if state == Qt.Checked:
             self.f_dir = os.path.dirname(self.file_name_ui)
 
     def onActivated(self, text):
+
         self.comb = text
         for i in range(len(self.settings["ui_data"])):
             if self.settings["ui_data"][i]["name"] == text:
@@ -98,11 +92,13 @@ class ExampleApp(QtWidgets.QMainWindow, ConverterUbuntuUI.Ui_Form):
                 self.file_name_py = self.settings["ui_data"][i]["py_path"]
 
     def button_click_remember(self):
+
         self.json_string = {"name": "%s" % self.name_lineEdit.text(), "ui_path": "%s" % (
             self.file_name_ui), "py_path": "%s" % (self.file_name_py)}
         self.json_data.append(self.json_string)
 
     def load_last_settings(self):
+
         try:
             self.settings_file = os.path.dirname(
                 __file__)+"/last_settings.json"
@@ -117,11 +113,13 @@ class ExampleApp(QtWidgets.QMainWindow, ConverterUbuntuUI.Ui_Form):
             pass
 
     def save_last_settings(self, settings_dir):
+
         self.last_settings = {"last_settings_file_dir": "%s" % (settings_dir)}
         with open(self.settings_file, "w") as write_file:
             json.dump(self.last_settings, write_file)
 
     def params_set(self):
+
         self.file_name_ui = ""
         self.file_name_py = ""
         self.json_string = ""
@@ -145,17 +143,17 @@ class ExampleApp(QtWidgets.QMainWindow, ConverterUbuntuUI.Ui_Form):
         self.remButton.clicked.connect(self.button_click_remember)
 
     def initUI(self):
+
         f_dir = os.path.dirname(__file__)
         self.setWindowIcon(QIcon(f_dir+'/reload.svg'))
         self.saveButton.setIcon(QIcon(f_dir+'/save.png'))
 
-
 def main():
+
     app = QtWidgets.QApplication(sys.argv) 
     window = ExampleApp() 
     window.show()
     app.exec_() 
-
 
 if __name__ == '__main__':
     main()
